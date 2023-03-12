9833e6cf19afaef0d84638a071f48e89a29b7091
Ian Kibet Leboo <88654576+StatisticianLeboo@users.noreply.github.com>
2023-03-12 15:54:03 +0300

    Add files via upload

---
+++ Programs/Counting_from_0_to_10_do_while_loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Program to count from zero to ten
+ * @n - the a parameter
+ * the program is using the do while loop
+ */
+
+int main ()
+{
+	int n;
+
+	do {
+	printf("%d \n", n);
+	n++;
+	}while(n<=10);
+return 0;
+}
---
+++ Programs/Counting_from_0_to_10_for_lloop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Program to count from zero to ten
+ * @n - the a parameter
+ * the program is using for loop
+ */
+
+
+int main ()
+{
+	int n;
+
+	for (n=0; n<=10; n++){
+	printf("%d \n", n);
+	}
+return 0;
+}
