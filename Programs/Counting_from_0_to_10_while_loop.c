625a31fc9a637c3c9e16d1e24693034bdb1ec635
StatisticianLeboo <lebooian61@gmail.com>
2023-03-12 15:51:06 +0300

    Loops for counting 0-10

---
+++ Programs/Counting_from_0_to_10_while_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Program to count from zero to ten
+ * @n - the a parameter
+ * the program is using while loop
+ */
+
+
+int main ()
+{
+	int n=0;
+
+	while (n<=10){
+	printf("%d \n", n);
+	n++;
+	}
+return 0;
+}
